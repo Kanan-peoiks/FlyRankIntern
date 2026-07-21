4a75acd500e9d2d5a83b2d3eac39e020fe9dad88
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 23:30:44 +0400

    first commit

---
+++ src/main/java/com/example/flyrankbackendintern/FlyRankBackendInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyrankbackendintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyRankBackendInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyRankBackendInternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flyrankbackendintern/FlyRankBackendInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyrankbackendintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyRankBackendInternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
